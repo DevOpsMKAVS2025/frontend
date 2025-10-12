a63b2a168be19ae2c207e5a2ced28a4cf7b60c8c
savic-a <anastasijas557@gmail.com>
2025-10-12 10:15:50 +0200

    feat: add host request reservation component

---
+++ src/app/modules/host-request-reservation/host-request-reservation.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HostRequestReservationComponent } from './host-request-reservation.component';
+
+describe('HostRequestReservationComponent', () => {
+  let component: HostRequestReservationComponent;
+  let fixture: ComponentFixture<HostRequestReservationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HostRequestReservationComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HostRequestReservationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
