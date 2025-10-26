34c2bd13d22f6857aca0d61ef31c2fe8c99ff1e1
vdevic01 <80548161+vdevic01@users.noreply.github.com>
2025-10-26 16:29:41 +0100

    Feature/login (#6)
    * feat: login page
    
    * feat: account page
    
    * feat: signup page
    
    * bugfix: jwt interceptor tests
    
    * bugfix: unit tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './modules/landing-page/landing-page.component';
 import { GuestRequestReservationComponent } from './modules/guest-request-reservation/guest-request-reservation.component';
 import { HostRequestReservationComponent } from './modules/host-request-reservation/host-request-reservation.component';
+import { LoginPageComponent } from './modules/login-page/login-page.component';
+import { SignupPageComponent } from './modules/signup-page/signup-page.component';
+import { AccountPageComponent } from './modules/account-page/account-page.component';
 
 export const routes: Routes = [
-    { path: 'guest/:type', component: GuestRequestReservationComponent },
-    { path: 'host/:type', component: HostRequestReservationComponent },
-    { path: '**', component: LandingPageComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'account', component: AccountPageComponent },
+  { path: 'guest/:type', component: GuestRequestReservationComponent },
+  { path: 'host/:type', component: HostRequestReservationComponent },
+  { path: '**', component: LandingPageComponent },
 ];
--- src/app/modules/toolbar/toolbar.component.spec.ts
+++ src/app/modules/toolbar/toolbar.component.spec.ts
@@ -1,23 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { ToolbarComponent } from './toolbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ToolbarComponent', () => {
-  let component: ToolbarComponent;
-  let fixture: ComponentFixture<ToolbarComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToolbarComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ToolbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      imports: [ToolbarComponent, RouterTestingModule],
+    }).compileComponents();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(ToolbarComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
