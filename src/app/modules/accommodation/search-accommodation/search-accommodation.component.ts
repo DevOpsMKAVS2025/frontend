8c56b98bbab64e5b61dcf240fa15014fed3da0f9
Katarina Vučić <kvucic6@gmail.com>
2025-10-26 18:37:36 +0100

    feat: implement host accommodations page

--- src/app/modules/accommodation/search-accommodation/search-accommodation.component.ts
+++ src/app/modules/accommodation/search-accommodation/search-accommodation.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Accommodation, ConvenieceType, PriceType } from '../../../models/accommodation';
 import { Observable, of } from 'rxjs';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AccommodationService } from '../../../services/accommodation-service';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-search-accommodation',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, FormsModule, MatTableModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatProgressSpinnerModule, MatNativeDateModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule, MatTableModule, MatInputModule,
+    MatButtonModule, MatDatepickerModule, MatProgressSpinnerModule, MatNativeDateModule,
+    MatSortModule],
   templateUrl: './search-accommodation.component.html',
   styleUrl: './search-accommodation.component.css'
 })
@@ -25,6 +28,9 @@ export class SearchAccommodationComponent implements OnInit {
   loading = false;
   PriceType = PriceType;
   displayedColumns: string[] = ['name', 'location', 'guests', 'conveniences', 'unitPrice', 'totalPrice'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     private fb: FormBuilder,
@@ -45,33 +51,38 @@ export class SearchAccommodationComponent implements OnInit {
 
   ngOnInit(): void {
     this.service.getAll().subscribe(r => this.results = r);
+    this.dataSource = new MatTableDataSource(this.results);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
   onSearch() {
-  const loc = this.form.value.location || '';
-  const guests = this.form.value.guests || 1;
-  const start = this.form.value.start || '';
-  const end = this.form.value.end || '';
+    const loc = this.form.value.location || '';
+    const guests = this.form.value.guests || 1;
+    const start = this.form.value.start || '';
+    const end = this.form.value.end || '';
 
-  if ((start && !end) || (!start && end)) {
-    alert('Please provide both start and end dates or neither.');
-    return;
-  }
-  if (start && end) {
-    const s = new Date(start);
-    const e = new Date(end);
-    if (e < s) {
-      alert('End date must be after start date.');
+    if ((start && !end) || (!start && end)) {
+      alert('Please provide both start and end dates or neither.');
       return;
     }
-  }
+    if (start && end) {
+      const s = new Date(start);
+      const e = new Date(end);
+      if (e < s) {
+        alert('End date must be after start date.');
+        return;
+      }
+    }
 
-  this.loading = true;
-  this.service.search(loc, guests, start, end)
-    .subscribe(res => {
-      this.results = res;
-      this.loading = false;
-    }, () => this.loading = false);
+    this.loading = true;
+    this.service.search(loc, guests, start, end)
+      .subscribe(res => {
+        this.results = res;
+        this.loading = false;
+      }, () => this.loading = false);
   }
 
   showConveniences(acc: Accommodation) {
@@ -79,6 +90,6 @@ export class SearchAccommodationComponent implements OnInit {
   }
 
   calculatePrices(acc: Accommodation, startDate: string, endDate: string, guests: number) {
-    return { 'total': 100, 'unitPrice': 50, 'priceType': 'per night'};
+    return { 'total': 100, 'unitPrice': 50, 'priceType': 'per night' };
   }
 }
