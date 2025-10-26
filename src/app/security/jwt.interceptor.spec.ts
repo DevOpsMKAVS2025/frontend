2468783b9f834877f2a84e70ea4ade99ef9e2379
vdevic01 <vlada.devic.2001@gmail.com>
2025-10-26 16:11:08 +0100

    bugfix: jwt interceptor tests

--- src/app/security/jwt.interceptor.spec.ts
+++ src/app/security/jwt.interceptor.spec.ts
@@ -1,14 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { JwtInterceptor } from './jwt.interceptor';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
-import { jwtInterceptor } from './jwt.interceptor';
-
-describe('jwtInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => jwtInterceptor(req, next));
+describe('JwtInterceptor', () => {
+  let interceptor: JwtInterceptor;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        JwtInterceptor,
+        { provide: UserService, useValue: {} }, // mock or stub if needed
+        { provide: Router, useValue: { navigate: jasmine.createSpy('navigate') } },
+      ],
+    });
+    interceptor = TestBed.inject(JwtInterceptor);
   });
 
   it('should be created', () => {
